96b916c21af4228b6854e8d91d6ee89787476404
krigjo25 <krigjo25@gmail.com>
2024-12-11 09:09:41 +0100

    Initializing the repository

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    public void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
